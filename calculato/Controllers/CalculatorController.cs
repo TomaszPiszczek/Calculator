36a9fe51cfd0885b2f671aeb1a7f77f704388bf7
TomaszPiszczek <115466543+TomaszPiszczek@users.noreply.github.com>
2023-10-07 20:48:33 +0200

    Dodaj pliki projektów.

---
+++ calculato/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace calculato.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double num1, double num2, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "Add":
+                    result = num1 + num2;
+                    break;
+                case "Subtract":
+                    result = num1 - num2;
+                    break;
+                case "Multiply":
+                    result = num1 * num2;
+                    break;
+                case "Divide":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Cannot divide by zero";
+                        return View("Index");
+                    }
+                    break;
+            }
+
+            ViewBag.Result = result;
+            return View("Index");
+        }
+    }
+}
---
+++ calculato/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace calculato.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double num1, double num2, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "Add":
+                    result = num1 + num2;
+                    Console.WriteLine("kurwa");
+                    break;
+                case "Subtract":
+                    result = num1 - num2;
+                    break;
+                case "Multiply":
+                    result = num1 * num2;
+                    break;
+                case "Divide":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Cannot divide by zero";
+                        return View("Index");
+                    }
+                    break;
+            }
+
+            ViewBag.Result = result;
+            return View("Index");
+        }
+    }
+}
